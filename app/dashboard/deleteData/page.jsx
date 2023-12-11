3ea65585f4f9e41db986bbfcc7f55904fbc8f56b
Abdalaziz Mohamed <124760094+Abdalaziz0486m@users.noreply.github.com>
2023-12-11 16:19:57 +0200

    Add delete-border class to globals.css

--- app/dashboard/deleteData/page.jsx
+++ app/dashboard/deleteData/page.jsx
@@ -50,7 +50,6 @@ export default function DeleteData() {
             <div className="col-12 mb-5">
               <h2 className="text-center fs-1">حذف البيانات</h2>
             </div>
-
             <div className="col-12">
               <div className="row">
                 <div className="col-12 col-md-6 mb-3">
@@ -94,113 +93,130 @@ export default function DeleteData() {
               </div>
             </div>
 
-            <div className="col-12">
-              <div className="row justify-content-center align-items-center">
-                <div className="col-12  mb-3">
-                  <div className="card border-danger pb-3 px-2">
-                    <div className="row justify-content-center align-items-center">
-                      <div className="col-md-3"></div>
-                      <div className="col-12 col-md-6 ">
-                        <div className="card-body">
-                          <h5 className="card-title text-center">
-                            حذف بيانات الطلاب
-                          </h5>
-                          <form action="">
-                            <div class="form-floating mb-3">
-                              <input
-                                type="search"
-                                class="form-control"
-                                id="studentSearch"
-                                placeholder="ابحث باسم الطالب"
-                                value={searchQuery}
-                                onChange={(event) =>
-                                  setSearchQuery(event.target.value)
-                                }
-                              />
-                              <label for="studentSearch">
-                                {" "}
-                                ابحث باسم الطالب{" "}
-                              </label>
-                            </div>
-                          </form>
-                        </div>
-                      </div>
-                      <div className="col-md-3"></div>
+            <div className="col-12 mt-5 mb-3">
+                <div className="row border-danger delete-border"> 
 
-                      {filteredStudents.map((student) => (
-                        <div className="col-12 col-md-6 mb-3">
-                          <div className="card border-info">
-                            <div className="card-header border-warning">
-                              <div className="row justify-content-start align-items-center">
-                                <div className="col-lg-3  col-5">
-                                  <p className="mb-0"> اسم الطالب </p>
-                                </div>
-                                <div className="col-1">
-                                  <p className="mb-0"> : </p>
-                                </div>
-                                <div className="col-lg-8  col-12">
-                                  <p className="mb-0">{student.name}</p>
-                                </div>
-                              </div>
-                            </div>
+                <nav className="col-12">
+                  <ul class="nav justify-content-evenly">
+                    <li class="nav-item">
+                      <a class="nav-link fs-5 " aria-current="page" href="#">الطلاب</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link fs-5" href="#">المعلمين</a>
+                    </li>
+                  </ul>
+                </nav>
 
+                <div className="col-12">
+                  <div className="row justify-content-center align-items-center">
+                    <div className="col-12  mb-3">
+                      <div className="card  pb-3 px-2">
+                        <div className="row justify-content-center align-items-center">
+                          <div className="col-md-3"></div>
+                          <div className="col-12 col-md-6 ">
                             <div className="card-body">
-                              <div className="row align-items-center justify-content-start mb-2">
-                                <div className="col-lg-3  col-5">
-                                  <p className="card-text"> هوية الطالب </p>
-                                </div>
-                                <div className="col-1">
-                                  <p className="card-text"> : </p>
-                                </div>
-                                <div className="col-lg-8  col-sm-12">
-                                  <p className="card-text">
-                                    {" "}
-                                    {student.number}{" "}
-                                  </p>
-                                </div>
-                              </div>
-
-                              <div className="row align-items-center justify-content-start mb-2">
-                                <div className="col-lg-3  col-5">
-                                  <p className="card-text"> السنة الدراسية </p>
-                                </div>
-                                <div className="col-1">
-                                  <p className="card-text"> : </p>
-                                </div>
-                                <div className="col-lg-8  col-sm-12">
-                                  <p className="card-text"> {student.class}</p>
-                                </div>
-                              </div>
-
-                              <div className="row align-items-center justify-content-start mb-3 pb-2 border-bottom border-warning">
-                                <div className="col-lg-3  col-5">
-                                  <p className="card-text"> رقم الفصل </p>
-                                </div>
-                                <div className="col-1">
-                                  <p className="card-text"> : </p>
-                                </div>
-                                <div className="col-lg-8  col-sm-12">
-                                  <p className="card-text"> {student.year} </p>
-                                </div>
-                              </div>
-
+                              <h5 className="card-title text-center">
+                                حذف بيانات الطلاب
+                              </h5>
                               <form action="">
-                                <button
-                                  type="button"
-                                  className="btn btn-danger text-center m-auto d-block"
-                                  onClick={() => deleteStudent(student.id)}
-                                >
-                                  {" "}
-                                  حذف الطالب{" "}
-                                </button>
+                                <div class="form-floating mb-3">
+                                  <input
+                                    type="search"
+                                    class="form-control"
+                                    id="studentSearch"
+                                    placeholder="ابحث باسم الطالب"
+                                    value={searchQuery}
+                                    onChange={(event) =>
+                                      setSearchQuery(event.target.value)
+                                    }
+                                  />
+                                  <label for="studentSearch">
+                                    {" "}
+                                    ابحث باسم الطالب{" "}
+                                  </label>
+                                </div>
                               </form>
                             </div>
                           </div>
+                          <div className="col-md-3"></div>
+
+                          {filteredStudents.map((student) => (
+                            <div className="col-12 col-md-6 mb-3">
+                              <div className="card border-info">
+                                <div className="card-header border-warning">
+                                  <div className="row justify-content-start align-items-center">
+                                    <div className="col-lg-3  col-5">
+                                      <p className="mb-0"> اسم الطالب </p>
+                                    </div>
+                                    <div className="col-1">
+                                      <p className="mb-0"> : </p>
+                                    </div>
+                                    <div className="col-lg-8  col-12">
+                                      <p className="mb-0">{student.name}</p>
+                                    </div>
+                                  </div>
+                                </div>
+
+                                <div className="card-body">
+                                  <div className="row align-items-center justify-content-start mb-2">
+                                    <div className="col-lg-3  col-5">
+                                      <p className="card-text"> هوية الطالب </p>
+                                    </div>
+                                    <div className="col-1">
+                                      <p className="card-text"> : </p>
+                                    </div>
+                                    <div className="col-lg-8  col-sm-12">
+                                      <p className="card-text">
+                                        {" "}
+                                        {student.number}{" "}
+                                      </p>
+                                    </div>
+                                  </div>
+
+                                  <div className="row align-items-center justify-content-start mb-2">
+                                    <div className="col-lg-3  col-5">
+                                      <p className="card-text"> السنة الدراسية </p>
+                                    </div>
+                                    <div className="col-1">
+                                      <p className="card-text"> : </p>
+                                    </div>
+                                    <div className="col-lg-8  col-sm-12">
+                                      <p className="card-text"> {student.class}</p>
+                                    </div>
+                                  </div>
+
+                                  <div className="row align-items-center justify-content-start mb-3 pb-2 border-bottom border-warning">
+                                    <div className="col-lg-3  col-5">
+                                      <p className="card-text"> رقم الفصل </p>
+                                    </div>
+                                    <div className="col-1">
+                                      <p className="card-text"> : </p>
+                                    </div>
+                                    <div className="col-lg-8  col-sm-12">
+                                      <p className="card-text"> {student.year} </p>
+                                    </div>
+                                  </div>
+
+                                  <form action="">
+                                    <button
+                                      type="button"
+                                      className="btn btn-danger text-center m-auto d-block"
+                                      onClick={() => deleteStudent(student.id)}
+                                    >
+                                      {" "}
+                                      حذف الطالب{" "}
+                                    </button>
+                                  </form>
+                                </div>
+                              </div>
+                            </div>
+                          ))}
                         </div>
-                      ))}
+                      </div>
                     </div>
                   </div>
                 </div>
+
               </div>
             </div>
           </div>
