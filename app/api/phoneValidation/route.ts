b3267db23034775337418ffb90c6f7037b68fc3d
Omar Ehab <oehab558@gmail.com>
2023-12-28 11:53:03 +0200

    Update instance_id in phoneValidation and sentMessageToTeacher routes

--- app/api/phoneValidation/route.ts
+++ app/api/phoneValidation/route.ts
@@ -49,7 +49,7 @@ export const POST = async (req: Request) => {
       number: parentNumber,
       type: "text",
       message: `كود التأكيد : ${code}`,
-      instance_id: "657082ACCE9CC",
+      instance_id: "658D421F79AE1",
       access_token: "657080406f853",
     });
     const url = "https://wa.karzoun.app/api/send?" + params.toString();
