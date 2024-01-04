a8d2fac6572dffe3be1715deeebddfed9a7c529d
Omar Ehab <oehab558@gmail.com>
2024-01-04 13:19:21 +0200

    Commented out unused code in students.ts

--- lib/forms.ts
+++ lib/forms.ts
@@ -1,307 +1,307 @@
-import prisma from "./db";
+// import prisma from "./db";
 
-// make function that returns all forms
+// // make function that returns all forms
 
-export const getAllForms = async () => {
-  try {
-    const data = await prisma.form.findMany();
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getAllForms = async () => {
+//   try {
+//     const data = await prisma.form.findMany();
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that returns one form
+// // make function that returns one form
 
-export const getForm = async (id: string) => {
-  try {
-    const data = await prisma.form.findUnique({
-      where: {
-        id,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getForm = async (id: string) => {
+//   try {
+//     const data = await prisma.form.findUnique({
+//       where: {
+//         id,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
 
-// make function that deletes one form
+// // make function that deletes one form
 
-export const deleteForm = async (id: string) => {
-  try {
-    const data = await prisma.form.delete({
-      where: {
-        id,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const deleteForm = async (id: string) => {
+//   try {
+//     const data = await prisma.form.delete({
+//       where: {
+//         id,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that updates one form
+// // make function that updates one form
 
-export const updateForm = async (id: string, body: any) => {
-  try {
-    const { studentId, reason, attachment, parentNumber, verificationCode, status, approval } = body;
-    const data = await prisma.form.update({
-      where: {
-        id,
-      },
-      data: {
-        studentId,
-        reason,
-        attachment,
-        parentNumber,
-        verificationCode,
-        status,
-        approval,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const updateForm = async (id: string, body: any) => {
+//   try {
+//     const { studentId, reason, attachment, parentNumber, verificationCode, status, approval } = body;
+//     const data = await prisma.form.update({
+//       where: {
+//         id,
+//       },
+//       data: {
+//         studentId,
+//         reason,
+//         attachment,
+//         parentNumber,
+//         verificationCode,
+//         status,
+//         approval,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that adds new form
+// // make function that adds new form
 
-export const addForm = async (body: any) => {
-  try {
-    const { studentId, reason, attachment, parentNumber, verificationCode, status, approval } = body;
-    const data = await prisma.form.create({
-      data: {
-        studentId,
-        reason,
-        attachment,
-        parentNumber,
-        verificationCode,
-        status,
-        approval,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const addForm = async (body: any) => {
+//   try {
+//     const { studentId, reason, attachment, parentNumber, verificationCode, status, approval } = body;
+//     const data = await prisma.form.create({
+//       data: {
+//         studentId,
+//         reason,
+//         attachment,
+//         parentNumber,
+//         verificationCode,
+//         status,
+//         approval,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that returns all forms by studentId
+// // make function that returns all forms by studentId
 
-export const getFormsByStudentId = async (studentId: string) => {
-  try {
-    const data = await prisma.form.findMany({
-      where: {
-        studentId,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getFormsByStudentId = async (studentId: string) => {
+//   try {
+//     const data = await prisma.form.findMany({
+//       where: {
+//         studentId,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that returns all forms by parentNumber
+// // make function that returns all forms by parentNumber
 
-export const getFormsByParentNumber = async (parentNumber: string) => {
-  try {
-    const data = await prisma.form.findMany({
-      where: {
-        parentNumber,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getFormsByParentNumber = async (parentNumber: string) => {
+//   try {
+//     const data = await prisma.form.findMany({
+//       where: {
+//         parentNumber,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that returns all forms by status
+// // make function that returns all forms by status
 
-export const getFormsByStatus = async (status: string) => {
-  try {
-    const data = await prisma.form.findMany({
-      where: {
-        status,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getFormsByStatus = async (status: string) => {
+//   try {
+//     const data = await prisma.form.findMany({
+//       where: {
+//         status,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that returns all forms by approval
+// // make function that returns all forms by approval
 
-export const getFormsByApproval = async (approval: string) => {
-  try {
-    const data = await prisma.form.findMany({
-      where: {
-        approval,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getFormsByApproval = async (approval: string) => {
+//   try {
+//     const data = await prisma.form.findMany({
+//       where: {
+//         approval,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that returns all forms by status and approval
+// // make function that returns all forms by status and approval
 
-export const getFormsByStatusAndApproval = async (status: string, approval: string) => {
-  try {
-    const data = await prisma.form.findMany({
-      where: {
-        status,
-        approval,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getFormsByStatusAndApproval = async (status: string, approval: string) => {
+//   try {
+//     const data = await prisma.form.findMany({
+//       where: {
+//         status,
+//         approval,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that returns all forms by studentId and status
+// // make function that returns all forms by studentId and status
 
-export const getFormsByStudentIdAndStatus = async (studentId: string, status: string) => {
-  try {
-    const data = await prisma.form.findMany({
-      where: {
-        studentId,
-        status,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getFormsByStudentIdAndStatus = async (studentId: string, status: string) => {
+//   try {
+//     const data = await prisma.form.findMany({
+//       where: {
+//         studentId,
+//         status,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that returns all forms by studentId and approval
+// // make function that returns all forms by studentId and approval
 
-export const getFormsByStudentIdAndApproval = async (studentId: string, approval: string) => {
-  try {
-    const data = await prisma.form.findMany({
-      where: {
-        studentId,
-        approval,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getFormsByStudentIdAndApproval = async (studentId: string, approval: string) => {
+//   try {
+//     const data = await prisma.form.findMany({
+//       where: {
+//         studentId,
+//         approval,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// get form code 
+// // get form code 
 
-export const getFormCode = async (id: string) => {
-  try {
-    const data = await prisma.form.findUnique({
-      where: {
-        id,
-      },
-      select: {
-        verificationCode: true,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getFormCode = async (id: string) => {
+//   try {
+//     const data = await prisma.form.findUnique({
+//       where: {
+//         id,
+//       },
+//       select: {
+//         verificationCode: true,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// add form code
+// // add form code
 
-export const addFormCode = async (id: string, verificationCode: string) => {
-  try {
-    const data = await prisma.form.update({
-      where: {
-        id,
-      },
-      data: {
-        verificationCode,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const addFormCode = async (id: string, verificationCode: string) => {
+//   try {
+//     const data = await prisma.form.update({
+//       where: {
+//         id,
+//       },
+//       data: {
+//         verificationCode,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// get form status
+// // get form status
 
-export const getFormStatus = async (id: string) => {
-  try {
-    const data = await prisma.form.findUnique({
-      where: {
-        id,
-      },
-      select: {
-        status: true,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getFormStatus = async (id: string) => {
+//   try {
+//     const data = await prisma.form.findUnique({
+//       where: {
+//         id,
+//       },
+//       select: {
+//         status: true,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// add form status
+// // add form status
 
-export const addFormStatus = async (id: string, status: string) => {
-  try {
-    const data = await prisma.form.update({
-      where: {
-        id,
-      },
-      data: {
-        status,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const addFormStatus = async (id: string, status: string) => {
+//   try {
+//     const data = await prisma.form.update({
+//       where: {
+//         id,
+//       },
+//       data: {
+//         status,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// get form approval
+// // get form approval
 
-export const getFormApproval = async (id: string) => {
-  try {
-    const data = await prisma.form.findUnique({
-      where: {
-        id,
-      },
-      select: {
-        approval: true,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const getFormApproval = async (id: string) => {
+//   try {
+//     const data = await prisma.form.findUnique({
+//       where: {
+//         id,
+//       },
+//       select: {
+//         approval: true,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// add form approval
+// // add form approval
 
-export const addFormApproval = async (id: string, approval: string) => {
-  try {
-    const data = await prisma.form.update({
-      where: {
-        id,
-      },
-      data: {
-        approval,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// export const addFormApproval = async (id: string, approval: string) => {
+//   try {
+//     const data = await prisma.form.update({
+//       where: {
+//         id,
+//       },
+//       data: {
+//         approval,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
--- lib/students.ts
+++ lib/students.ts
@@ -1,60 +1,60 @@
-import prisma from "./db";
+// import prisma from "./db";
 
-// make function that returns all students
-export const getAllStudents = async () => {
-  try {
-    const data = await prisma.student.findMany();
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// // make function that returns all students
+// export const getAllStudents = async () => {
+//   try {
+//     const data = await prisma.student.findMany();
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that returns one student
-export const getStudent = async (number: string) => {
-  try {
-    const data = await prisma.student.findUnique({
-      where: {
-        number,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// // make function that returns one student
+// export const getStudent = async (number: string) => {
+//   try {
+//     const data = await prisma.student.findUnique({
+//       where: {
+//         number,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that deletes one student
-export const deleteStudent = async (number: string) => {
-  try {
-    const data = await prisma.student.delete({
-      where: {
-        number,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// // make function that deletes one student
+// export const deleteStudent = async (number: string) => {
+//   try {
+//     const data = await prisma.student.delete({
+//       where: {
+//         number,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-// make function that updates one student
-export const updateStudent = async (number: string, body: any) => {
-  try {
-    const { name, class: className, year, parentNumber } = body;
-    const data = await prisma.student.update({
-      where: {
-        number,
-      },
-      data: {
-        name,
-        class: className,
-        year,
-        parentNumber,
-      },
-    });
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
+// // make function that updates one student
+// export const updateStudent = async (number: string, body: any) => {
+//   try {
+//     const { name, class: className, year, parentNumber } = body;
+//     const data = await prisma.student.update({
+//       where: {
+//         number,
+//       },
+//       data: {
+//         name,
+//         class: className,
+//         year,
+//         parentNumber,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
