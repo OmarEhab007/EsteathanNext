e393e1899e9ba5c79a566f11e259dd58e375101c
Abdalaziz Mohamed <124760094+Abdalaziz0486m@users.noreply.github.com>
2024-03-05 09:45:21 +0200

    Fix bug in login functionality

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function layout() {
+  return (
+    <>
+      <head>
+        <meta
+          name="facebook-domain-verification"
+          content="mbajg6kdppwxvuszt3u5s9bu9p6mxg"
+        />
+      </head>
+    </>
+  );
+}
